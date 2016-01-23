1ffe390fe865cede0bf277cd3be38c09b7522fa0
Adam Knox <adam.w.knox@gmail.com>
2016-01-23 18:19:56 -0500

    roundtrip

--- background.js
+++ background.js
@@ -3,18 +3,17 @@ console.log('background script loaded');
 chrome.browserAction.onClicked.addListener(function(tab) {
     var params = {active: true, currentWindow:true};
     chrome.tabs.query(params, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id,{}, function(resp){
-            console.log('resp',resp);
+        var msg = {message: 'scrape'};
+        // Send the click to the content script
+        chrome.tabs.sendMessage(tabs[0].id, msg, function(resp){
+            console.log('bg resp', resp)
+            // COMMENT
+            getTrackDetails(resp.href, function(details){
+                console.log('details',details);
+                details.message = 'track-details';
+                chrome.tabs.sendMessage(tabs[0].id, details);
+            });
         });
-
-        //TODO: USE HREF FROM CONTENT SCRIPT
-        var href = "https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR";
-
-        getTrackDetails(href, function(details){
-            console.log('details',details);
-            // TODO: SEND A MESSAGE TO THE WINDOW TO INSERT SOME HTML
-        });
-
     });
 
 });
@@ -32,18 +31,40 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
 
 
 function getTrackDetails (href, cb) {
+
     fetchTrack(href, function(doc) {
         var details = {};
-        details.description = getMetaContentByName('description', doc);
+        addMetaProps(details, doc);
+        addProps(details, doc);
+
         cb(details);
     });
 }
 
+function addMetaProps(details, doc) {
+    var props = [
+        {prop:'description',    label:'description'},
+        {prop:'og:title',       label:'title'},
+        {prop:'og:image',       label:'album_art'},
+        {prop:'music:duration', label:'duration'},
+        {prop:'music:album',    label:'album_href'},
+        {prop:'music:musician', label:'artist_href'},
+    ];
 
-// https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR
+    props.forEach(function(prop) {
+        details[prop.label] = getMetaContentByName(prop.prop, doc);
+    });
+}
+
+function addProps(details, doc) {
+    var query = doc.querySelectorAll('a.owner-name');
+    details.artist = query[0].innerText;
+    details.album = query[1].innerText;
+}
+
+
+// href = https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR
 function fetchTrack(href, cb) {
-    console.log(href);
-    if (!href) { return false; }
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", href, true);
@@ -59,5 +80,6 @@ function fetchTrack(href, cb) {
 
 
 function getMetaContentByName(name, doc){
+    console.log('meta props' + name);
     return doc.querySelector('meta[property="'+name+'"]').getAttribute('content')
 }
--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,26 @@
 console.log('content script loaded');
 
 // Monitor for button click
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    var href = getSpotifyUrl();
-    sendResponse({href: href});
-  });
+chrome.runtime.onMessage.addListener(listener);
+
+function listener(request, sender, sendResponse) {
+    console.log('request', request);
+    if (request.message === 'scrape') {
+        scrapeHandler(sendResponse);
+    } else {
+        console.log('other');
+    }
+}
+
+function scrapeHandler(sendResponse) {
+    var msg = {
+        href: getSpotifyUrl()
+    };
+    console.log(msg);
+    sendResponse(msg);
+}
 
 function getSpotifyUrl() {
     var anchors = document.activeElement.getElementsByTagName('a');
     return anchors[0].href
 }
-

d4660bbda168c04656b68432f39dddc04d7517d9
Adam Knox <adam.w.knox@gmail.com>
2016-01-23 14:42:56 -0500

    got xhr working

--- background.js
+++ background.js
@@ -5,5 +5,42 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var params = {active: true, currentWindow:true};
     chrome.tabs.query(params, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id,{}, function(){});
+
+        //TODO: MOVE INTO CALLBACK
+        //TODO: USE HREF FROM CONTENT SCRIPT
+        fetchTrackDetails("https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR");
+
+        // TODO: SEND A MESSAGE TO THE WINDOW TO INSERT SOME HTML
     });
+
 });
+
+// We need wget headers to get a usable response
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
+    for(var i=0; i < details.requestHeaders.length; ++i){
+        if(details.requestHeaders[i].name === "User-Agent"){
+            details.requestHeaders[i].value = 'Wget/1.12';
+            break;
+        }
+    }
+    return {requestHeaders: details.requestHeaders};
+}, {urls: ["https://open.spotify.com/*"]}, ["blocking", "requestHeaders"]);
+
+
+// https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR
+function fetchTrackDetails(href) {
+    console.log(href);
+    if (!href) { return false; }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", href, true);
+    xhr.responseType = 'document';
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        console.log(xhr.response);
+        console.log(xhr.response.head);
+        // TODO: PARSE OUT THE VARS WE WANT FROM THE RESPONSE
+      }
+    }
+    xhr.send();
+}
--- contentscript.js
+++ contentscript.js
@@ -3,24 +3,31 @@ console.log('content script loaded');
 // Monitor for button click
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    spotifyEmbed();
+    // spotifyEmbed();
+    //TODO: SEND RESPONSE OF HREFS TO FETCH
   });
 
 function spotifyEmbed() {
     var anchors = document.activeElement.getElementsByTagName('a');
     for (var i = 0; anchors[i]; i++) {
         var anchor = anchors[i];
-        var href = anchor.href;
-        console.log(href);
-        fetchTrackDetails(href);
+        var trackDetails = fetchTrackDetails(anchor.href);
     }
 }
 
 // https://open.spotify.com/track/3qDUJ5FQpEMvfg1ihGEWkR
 function fetchTrackDetails(href) {
+    console.log(href);
     if (!href) { return false; }
 
-
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", href, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        console.log(xhr.responseText);
+      }
+    }
+    xhr.send();
 }
 
 function getMetaContentByName(name){
